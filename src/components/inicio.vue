77b3f3fe94732d5ed4fbe891686c2007c543ce9c
Oriol_simon <orioltunino1234@gmail.com>
2023-08-08 12:40:21 -0500

    agregando

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -1,13 +1,12 @@
 
 <template>
     <div>
-        
         <v-card class="ma-5 px-3">
             <v-card-title>
                 Registros recientes
             </v-card-title>
             <v-row>
-                <v-col cols="12" md="4">
+                <v-col cols="12" sm="4">
                     <v-card>
                         <form action=""
                         @submit.prevent="enviardato()"
@@ -35,14 +34,23 @@
                         </form>
                     </v-card>             
                 </v-col>
-                <v-col cols="12" md="8">
+                <v-col cols="12" sm="8">
                     <v-card-text>   
                         <v-data-table
                             :headers="headers"
                             :items="registros"
                             :items-per-page="-1"
                             class="elevation-1"
-                        ></v-data-table>
+                        >
+                            <template v-slot:[`item.fecha`]="{ item }">
+                                <v-chip
+                                    color="warning"
+                                    dark
+                                >
+                                {{ get_fecha(item.fecha) }}
+                                </v-chip>
+                            </template>
+                        </v-data-table>
                     </v-card-text>
                 </v-col>
             </v-row>
@@ -52,7 +60,7 @@
 </template>
 <script>
 import { firestore } from '@/plugins/firebase.js';
-import { collection, onSnapshot,addDoc  } from 'firebase/firestore';
+import { collection, onSnapshot,doc,updateDoc,arrayUnion  } from 'firebase/firestore';
 export default{
     name: 'InicioPage',
     data(){
@@ -76,24 +84,42 @@ export default{
         enviardato(){
             this.loader=true;
             this.form.fecha=new Date();
-            const reg=collection(firestore,'registros');
-            addDoc(reg, this.form)
-            .then((docRef) => {
-                console.log('Documento agregado con ID:', docRef.id);
-                this.loader=false;
+            const reg=doc(collection(firestore,'registros'),'semana1');
+
+            updateDoc(reg,{
+                registros:arrayUnion(this.form)
+            })
+            .then(() => {
+                console.log('Datos agregados exitosamente al documento en Firebase');
                 this.form.cantidad='';
                 this.form.precio='';
             })
             .catch((error) => {
-                console.error('Error al agregar el documento:', error);
+                console.error('Error al agregar datos al documento:', error);
+            })
+            .finally(()=>{
+                this.loader=false;
             });
+
         },
+        get_fecha(fecha){
+            const opcionesDeFormato = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+            const fechaFormateada = new Date(fecha.seconds * 1000).toLocaleDateString('es-ES', opcionesDeFormato);
+            return fechaFormateada;
+        }
     },
     async mounted() {
-        const regs=collection(firestore,'registros')
+        const regs=doc(collection(firestore,'registros'),'semana1');
         
-        onSnapshot(regs, (querySnapshot) => {
-          this.registros= querySnapshot.docs.map((doc) => doc.data());
+        onSnapshot(regs, (docSnapshot) => {
+            if (docSnapshot.exists()) {
+                const datosDelDocumento = docSnapshot.data();
+                this.registros=datosDelDocumento.registros;
+                console.log('Datos actualizados del documento:', datosDelDocumento);
+            } else {
+                console.log('El documento ya no existe');
+            }
+          //this.registros= querySnapshot.docs.map((doc) => doc.data());
         }, (error) => {
             console.error('Error al escuchar los cambios:', error);
         });
