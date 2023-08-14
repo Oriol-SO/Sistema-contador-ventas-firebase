f9a7d4ad651c39ebc10651a1c7b66f8d032c1d93
Oriol_simon <orioltunino1234@gmail.com>
2023-08-14 14:37:26 -0500

    agregando cochachi gil

--- src/components/historial.vue
+++ src/components/historial.vue
@@ -7,11 +7,33 @@
             <v-card-text>
                 <v-row>
                     <v-col cols="12" sm="4">
-                        <v-select></v-select>
+                        <v-autocomplete
+                        :items="grupos"
+                        v-model="grupo"
+                        item-text="inicio"
+                        return-object
+                        ></v-autocomplete>
                     </v-col>
                     <v-col cols="12" sm="8">
+                        <v-card-title >
+                        <v-chip large color="orange" class="text-h5 ">CT: <strong>{{ sumacantidad }}</strong></v-chip>
+                        <v-chip large color="yellow" class="text-h5 ml-5">PT: <strong>{{ sumaprecio }}</strong></v-chip>
+                        <!--span class="ml-auto"> id: {{ this.iddocumento }}</span-->
+                        </v-card-title>
                         <v-data-table
-                        
+                            :headers="headers"
+                            :items="grupo?.registros"
+                            :items-per-page="-1"
+                            class="elevation-1"
+                        >
+                            <template v-slot:[`item.fecha`]="{ item }">
+                                <v-chip
+                                    color="purple"
+                                    dark
+                                >
+                                {{ get_fecha(item.fecha) }}
+                                </v-chip>
+                            </template>
                         ></v-data-table>
                     </v-col>
                 </v-row>
@@ -20,7 +42,69 @@
     </div>
 </template>
 <script>
+import { firestore } from '@/plugins/firebase.js';
+import { collection, query, orderBy, getDocs } from 'firebase/firestore';
 export default {
-    name:'HistorialPage'
+    name:'HistorialPage',
+    data(){
+        return{
+            grupo:null,
+            grupos:[],
+            headers: [
+                
+                { text: 'cantidad', value: 'cantidad' },
+                { text: 'precio', value: 'precio' },
+                { text: 'fecha', value: 'fecha' },
+            ],
+        }
+    },methods:{
+        get_documents(){
+            const histo=collection(firestore,'registros');
+
+            const consulta = query(histo, orderBy('inicio', 'desc'));
+
+            getDocs(consulta)
+            .then(querySnapshot => {
+                const documentos = [];
+
+                querySnapshot.forEach(doc => {
+                    documentos.push({
+                        id: doc.id,
+                        registros: doc.data().registros,
+                        inicio:'Grupo de '+this.get_fecha(doc.data().inicio)
+                    });
+                });
+
+                this.grupos=documentos
+            })
+            .catch(error => {
+                console.error('Error al obtener los documentos de la colección:', error);
+            });
+        },
+        
+        get_fecha(fecha){
+            const opcionesDeFormato = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+            const fechaFormateada = new Date(fecha.seconds * 1000).toLocaleDateString('es-ES', opcionesDeFormato);
+            return fechaFormateada;
+        },
+    },mounted(){
+        this.get_documents();
+    },computed:{
+        sumacantidad(){
+            let sumcant=0;
+            this.grupo?.registros.forEach(element=>{
+                sumcant=sumcant+parseFloat(element.cantidad)
+            })
+            return sumcant;
+            
+        },
+        sumaprecio(){
+            let sumpre=0;
+            this.grupo?.registros.forEach(element=>{
+                sumpre=sumpre+(parseFloat(element.precio))
+            })
+            return sumpre;
+        }
+    }
 }
 </script>
--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -6,6 +6,7 @@
                 Registros recientes
             </v-card-title>
             <v-card-title>
+                <v-btn color="purple" class="white--text" @click="resetear()" >Resetear tabla</v-btn>
                 <v-btn class="ml-auto" color="warning" to="/historial">HISTORIAL</v-btn>
             </v-card-title>
             <v-row>
@@ -38,6 +39,11 @@
                     </v-card>             
                 </v-col>
                 <v-col cols="12" sm="8">
+                    <v-card-title >
+                        <v-chip large color="orange" class="text-h5 ">CT: <strong>{{ sumacantidad }}</strong></v-chip>
+                        <v-chip large color="yellow" class="text-h5 ml-5">PT: <strong>{{ sumaprecio }}</strong></v-chip>
+                        <!--span class="ml-auto"> id: {{ this.iddocumento }}</span-->
+                    </v-card-title>
                     <v-card-text>   
                         <v-data-table
                             :headers="headers"
@@ -53,8 +59,12 @@
                                 {{ get_fecha(item.fecha) }}
                                 </v-chip>
                             </template>
+                            <template v-slot:[`item.del`]="{ item }">
+                               <v-btn @click="eliminar(item,)" small color="error" rounded>Quitar</v-btn>
+                            </template>
                         </v-data-table>
                     </v-card-text>
+                    
                 </v-col>
             </v-row>
         </v-card>
@@ -62,7 +72,7 @@
 </template>
 <script>
 import { firestore } from '@/plugins/firebase.js';
-import { collection, onSnapshot,doc,updateDoc,arrayUnion  } from 'firebase/firestore';
+import { collection, onSnapshot,doc,updateDoc,arrayUnion,query, orderBy, limit,getDocs,addDoc,arrayRemove  } from 'firebase/firestore';
 export default{
     name: 'InicioPage',
     data(){
@@ -72,21 +82,44 @@ export default{
                 { text: 'cantidad', value: 'cantidad' },
                 { text: 'precio', value: 'precio' },
                 { text: 'fecha', value: 'fecha' },
+                { text: 'Del', value: 'del' },
             ],
 
             form:{
-                cantidad:'',
-                precio:'',
-                fecha:'',
+                cantidad:null,
+                precio:null,
+                fecha:null,
             },
             loader:false,
-            registros:[]
+            registros:[],
+
+            ultimoDocumento:null,
+            iddocumento:null,
+
+            
         }
-    },methods:{
+    },computed:{
+        sumacantidad(){
+            let sumcant=0;
+            this.registros.forEach(element=>{
+                sumcant=sumcant+parseFloat(element.cantidad)
+            })
+            return sumcant;
+            
+        },
+        sumaprecio(){
+            let sumpre=0;
+            this.registros.forEach(element=>{
+                sumpre=sumpre+(parseFloat(element.precio))
+            })
+            return sumpre;
+        }
+    },    
+    methods:{
         enviardato(){
             this.loader=true;
             this.form.fecha=new Date();
-            const reg=doc(collection(firestore,'registros'),'semana1');
+            const reg=doc(collection(firestore,'registros'),this.iddocumento);
 
             updateDoc(reg,{
                 registros:arrayUnion(this.form)
@@ -108,24 +141,70 @@ export default{
             const opcionesDeFormato = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
             const fechaFormateada = new Date(fecha.seconds * 1000).toLocaleDateString('es-ES', opcionesDeFormato);
             return fechaFormateada;
+        },
+        async obtener_ultimo_grupo(){
+            const end=collection(firestore,'registros');
+
+            const q = query(end, orderBy('inicio', 'desc'), limit(1));
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+                this.ultimoDocumento = querySnapshot.docs[0].data();
+                return querySnapshot.docs[0].id; 
+            } else {
+                console.log('La colección está vacía.');
+                return null;
+            }
+          
+        },
+       async  resetear(){
+            if(!confirm('¿Estas seguro de realizar esta acción?')){
+                return ;
+            }
+            const reset = collection(firestore, 'registros');
+            const nuevoDocumentoRef= await addDoc(reset, {
+                inicio: new Date(),
+                registros:[],
+            });
+
+            console.log('Nuevo documento creado con ID:', nuevoDocumentoRef.id);
+            this.registros=[];
+            this.get_datos_registro();   
+        },
+
+        async get_datos_registro(){
+            this.iddocumento=await this.obtener_ultimo_grupo();
+            const regs=doc(collection(firestore,'registros'),this.iddocumento);  
+            onSnapshot(regs, (docSnapshot) => {
+                if (docSnapshot.exists()) {
+                    const datosDelDocumento = docSnapshot.data();
+                    this.registros=datosDelDocumento.registros;
+                    
+                } else {
+                    console.log('El documento ya no existe');
+                }
+            //this.registros= querySnapshot.docs.map((doc) => doc.data());
+            }, (error) => {
+                console.error('Error al escuchar los cambios:', error);
+            });
+        },
+        
+        
+        eliminar(item){
+            console.log(item)
+            const del=doc(collection(firestore,'registros'),this.iddocumento);
+            updateDoc(del, {
+                registros: arrayRemove(item)
+            })
+            .then(() => {
+                console.log('Elemento removido del array en la actualización.');
+            })
+            .catch(error => {
+                console.error('Error al remover el elemento del array:', error);
+            });
         }
     },
-    async mounted() {
-        const regs=doc(collection(firestore,'registros'),'semana1');
-        
-        onSnapshot(regs, (docSnapshot) => {
-            if (docSnapshot.exists()) {
-                const datosDelDocumento = docSnapshot.data();
-                this.registros=datosDelDocumento.registros;
-            } else {
-                console.log('El documento ya no existe');
-            }
-          //this.registros= querySnapshot.docs.map((doc) => doc.data());
-        }, (error) => {
-            console.error('Error al escuchar los cambios:', error);
-        });
-        
-      
+    mounted() {
+        this.get_datos_registro();      
         
     }
 }
